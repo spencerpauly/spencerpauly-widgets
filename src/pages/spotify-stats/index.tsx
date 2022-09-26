3de0086e4432454f78c3941232343124675da1b4
spencerpauly <spencerpauly@gmail.com>
2022-09-26 12:07:43 +0200

    chore: test modal in embed mode

--- src/pages/spotify-stats/index.tsx
+++ src/pages/spotify-stats/index.tsx
@@ -1,6 +1,15 @@
 import { getBlankLayout } from '@/layouts/BlankLayout';
 import SpotifyStatsPage from '@/modules/spotify-stats/SpotifyStatsPage';
 import { NextPageWithLayout } from '@/types/page';
+import {
+	Button,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+} from '@chakra-ui/react';
 import { useSession } from 'next-auth/react';
 import React from 'react';
 
@@ -9,7 +18,26 @@ interface Props {}
 const SpotifyStats: NextPageWithLayout<Props> = () => {
 	const session = useSession();
 
-	return <SpotifyStatsPage />;
+	return (
+		<>
+			<Modal isCentered isOpen={true} onClose={() => {}}>
+				{/* <ModalOverlay /> */}
+				<ModalContent>
+					<ModalHeader>Modal Title</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody></ModalBody>
+
+					<ModalFooter>
+						<Button colorScheme='blue' mr={3} onClick={() => {}}>
+							Close
+						</Button>
+						<Button variant='ghost'>Secondary Action</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+			<SpotifyStatsPage />
+		</>
+	);
 };
 
 SpotifyStats.getLayout = getBlankLayout();
