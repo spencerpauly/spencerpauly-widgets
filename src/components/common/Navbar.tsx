985098702d3ce269b5213c0ac423b82ec5dab8d7
spencerpauly <spencerpauly@gmail.com>
2023-05-20 15:13:57 -0500

    feat: updates navbar to match homepage

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -4,22 +4,19 @@ interface Props {}
 const Navbar: NextPageWithLayout<Props> = () => {
 	return (
 		<nav className='w-full bg-green-500 shadow'>
-			<div className='flex items-center max-w-7xl px-[40px] py-[24px] mx-auto space-x-12'>
+			<div className='flex items-center max-w-7xl px-[40px] py-[24px] mx-auto space-x-[38px]'>
 				<a href='https://spencerpauly.com/'>
 					<img src='/static/content/spencerpauly-logo.png' className='w-auto h-[42px]' />
 				</a>
 				<div className=' space-x-[32px] hidden md:flex'>
 					<div className='font-medium text-white'>
-						<a href='https://spencerpauly.com/about'>About</a>
+						<a href='https://spencerpauly.com/manifesto'>Manifesto</a>
 					</div>
 					<div className='font-medium text-white'>
 						<a href='https://spencerpauly.com/launches'>Products</a>
 					</div>
 					<div className='font-medium text-white'>
 						<a href='https://spencerpauly.com/tech'>Tech Blog</a>
-					</div>
-					<div className='font-medium text-white'>
-						<a href='https://spencerpauly.com/glossary'>Glossary</a>
 					</div>
 				</div>
 			</div>
