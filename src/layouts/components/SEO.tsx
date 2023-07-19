4027ea63dbe862ff7e76927987f41f3634f45f02
spencerpauly <spencerpauly@gmail.com>
2023-07-19 17:58:57 -0500

    chore: update favicon in prod

--- src/layouts/components/SEO.tsx
+++ src/layouts/components/SEO.tsx
@@ -1,20 +1,21 @@
 import { NextSeo } from 'next-seo';
+import Head from 'next/head';
 import Script from 'next/script';
 import React from 'react';
 
 export function SEO() {
 	return (
 		<>
-			<NextSeo
-				title={'Spencer Pauly | Widgets'}
-				description={''}
-				additionalLinkTags={[
-					{
-						rel: 'icon',
-						href: 'favicon.ico',
-					},
-				]}
-			/>
+			<NextSeo title={'Spencer Pauly | Widgets'} description={''} />
+			<Head>
+				<link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
+				<link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
+				<link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
+				<link rel='manifest' href='/favicon/site.webmanifest' />
+				<link rel='mask-icon' href='/favicon/safari-pinned-tab.svg' color='#5bbad5' />
+				<meta name='msapplication-TileColor' content='#da532c' />
+				<meta name='theme-color' content='#ffffff' />
+			</Head>
 			<Script src='/static/iframeResize.contentWindow.min.js' />
 		</>
 	);
